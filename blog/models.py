828be8c5e9b690c29f5cb3156a3f068ea4afe2f5
Maxim-Burkov <sup3r44ax@yandex.ru>
2022-03-27 13:38:09 +0300

    25.03

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,6 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
 
-    def __str__(self):
-        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+import sqlite3
+
+from django.contrib.auth.models import User
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+
+    user_name = User.objects.all()
+    print(user_name)
+
+
+    posts = Post.objects.all()
+    last_post_id = len(posts) - 1
+
+    post_id = last_post_id
+    post_title = posts[last_post_id].title
+    post_text = posts[last_post_id].text
+    post_data = posts[last_post_id].created_date
+
+
+    return render(request, 'blog/post_list.html', {'post_title': post_title, 'post_text': post_text,
+                'post_data': post_data, 'post_id': post_id})
