05476b244116c8a8845956501eb16c03c99fc2e1
Maxim-Burkov <sup3r44ax@yandex.ru>
2022-03-27 14:20:03 +0300

    25.03

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('/all_posts', views.all_post_list, name='all_post_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -22,3 +22,23 @@ def post_list(request):
 
     return render(request, 'blog/post_list.html', {'post_title': post_title, 'post_text': post_text,
                 'post_data': post_data, 'post_id': post_id})
+
+
+def all_post_list(request):
+    user_name = User.objects.all()
+    print(user_name)
+
+    posts = Post.objects.all()
+
+    for i in range(len(posts)):
+
+        last_post_id = len(posts) - 1
+
+        post_id = last_post_id
+        post_title = posts[last_post_id].title
+        post_text = posts[last_post_id].text
+        post_data = posts[last_post_id].created_date
+
+        return render(request, 'blog/all_post_list.html', {'post_title': post_title, 'post_text': post_text,
+                                                           'post_data': post_data, 'post_id': post_id})
+
