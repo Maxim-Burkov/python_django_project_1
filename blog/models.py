96ebe7ca2a420f7b48f74188943aba0232b9c0ef
Maxim-Burkov <sup3r44ax@yandex.ru>
2022-03-27 13:52:47 +0300

    25.03

--- blog/models.py
+++ blog/models.py
@@ -8,5 +8,4 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField()
-    published_date = models.DateTimeField()
 
